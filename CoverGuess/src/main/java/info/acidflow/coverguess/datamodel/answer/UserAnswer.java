92180e837a099a0117ec1782b5b726a3cd971ce5
Acid Flow <laturaze.paul+git@gmail.com>
2014-06-21 13:14:06 +0200

    Prefill some char todo

--- CoverGuess/src/main/java/info/acidflow/coverguess/datamodel/answer/UserAnswer.java
+++ CoverGuess/src/main/java/info/acidflow/coverguess/datamodel/answer/UserAnswer.java
@@ -1,27 +1,58 @@
 package info.acidflow.coverguess.datamodel.answer;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by paul on 01/06/14.
  */
 public class UserAnswer extends AbstractAnswer {
 
     private boolean[] mPrefilledPositions;
+    private ArrayList<String> mCharactersToGuess;
 
     public UserAnswer(String value) {
         super(value);
         mPrefilledPositions = new boolean[ value.length() ];
+        mCharactersToGuess = new ArrayList<String>();
         initAnswer( value );
-
+        initCharactersToGuess(value);
     }
 
     @Override
     public void initAnswer( String value ){
+        int countCharactersToGuess = 0;
+        int countPrefilled = 0;
         for(int i = 0; i < value.length(); i++ ){
             if( !Character.isLetterOrDigit( value.charAt(i) ) ){
                 setCharAt(i, value.charAt(i));
                 mPrefilledPositions[ i ] = true;
+                countPrefilled++;
+            }else{
+                countCharactersToGuess++;
             }
         }
+        if(countCharactersToGuess > 12 ){
+            prefillOthers(value, countPrefilled, countCharactersToGuess - 12 );
+        }
+    }
+
+    private void prefillOthers( String value, int countPrefilled, int remainingToFill ){
+        //TODO
+    }
+
+    public void initCharactersToGuess(String value) {
+        for( int i = 0; i < getAnswerLength(); i++ ){
+            if(!mPrefilledPositions[i]){
+                mCharactersToGuess.add( String.valueOf( value.charAt( i ) ) );
+            }
+        }
+    }
+
+    public ArrayList<String> getCharactersToGuess(){
+        return mCharactersToGuess;
     }
 
     @Override
